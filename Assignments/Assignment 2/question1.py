e69f541ed474ed2492622ffc25a5fe7a608910f8
Michael Buffone <44306347+BuffoneM@users.noreply.github.com>
2021-02-16 19:32:31 -0500

    +

--- Assignments/Assignment 2/question1.py
+++ Assignments/Assignment 2/question1.py
@@ -11,7 +11,24 @@
 # (inversions are 9 > 8,  9 > 4,  9 > 5,  8 > 4,  8 > 5)
 ###
 
-def countInversions(array, index):
+def countInversions(array):
+    if len(array) <= 1:
+        return
+    if(len(array) == 2):
+        if array[0] > array[1]:
+            print("Inversion pair:", array[0], ">", array[1])
+            return 1
+        else:
+            print("Inversion pair:", array[0], "!>", array[1])
+            return 0
+    
+    halfPoint = len(array) // 2
+    leftArray = array[:halfPoint]
+    rightArray = array[halfPoint:]
+    
+    return countInversions(leftArray) + countInversions(rightArray)
+
+def countInversions2(array, index):
     # -if the array is empty, return 0
     # -if we've compared all items, remove the first element in the
     #  array and restart at index 1 (We don't compare the item to itself)
@@ -19,23 +36,24 @@ def countInversions(array, index):
         return 0
     if(index == len(array)):
         print()
-        return countInversions(array[1:], 1)
+        return countInversions2(array[1:], 1)
     
     print(array, " at ", index, ": ", end="", sep="")    
 
     # recursive call- if inversion, add 1
     if(array[0] > array[index]):
         print(array[0], ">", array[index])
-        return 1 + countInversions(array, index + 1)
+        return 1 + countInversions2(array, index + 1)
     else:
         print(array[0], "!>", array[index])
-        return countInversions(array, index + 1)
+        return countInversions2(array, index + 1)
     
 def main():
     print("-----")
     print("The inversion pairs are:")
     array = [9, 8, 4, 5]
-    print("The number of inversions with the array", array, "is:", countInversions(array, 1))
+    print("The number of inversions with the array", array, "is:", countInversions(array))
+
     print("-----")
 
 if __name__ == "__main__":
