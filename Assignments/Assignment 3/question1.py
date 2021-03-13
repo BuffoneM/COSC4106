869b69ebfef2cb7355eba063f0915d57d5a02f20
Michael Buffone <michaelbuffonee@gmail.com>
2021-03-12 20:06:36 -0500

    +

---
+++ Assignments/Assignment 3/question1.py
@@ -0,0 +1,19 @@
+###
+# Michael Buffone
+# March 12th, 2021
+# COSC4106 Assignment 3 : Question 1
+#
+# Write a program to implement the Horspool’s algorithm. Your program should ask the user to enter
+# a text and a pattern, then output the following: 
+# (a) shift table   
+# (b) the searching result (whether the pattern is in the text or not)
+###
+
+def main():
+    print("-----")
+    
+    print("-----")
+
+
+if __name__ == "__main__":
+    main()
