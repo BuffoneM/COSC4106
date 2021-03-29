2666a71565638ea580bdc09a2e8b34bae898118d
Michael Buffone <44306347+BuffoneM@users.noreply.github.com>
2021-03-29 16:48:42 -0400

    +

--- Assignments/Assignment 4/question2.py
+++ Assignments/Assignment 4/question2.py
@@ -4,9 +4,18 @@
 # COSC4106 Assignment 4 : Question 2
 #
 ###
-     
+def recursiveSolution(workSchedule, n):
+
+    print("1234")
+
 def main():
     print("-----")
+    
+    n = 4
+    workSchedule = [[10, 1, 10, 10], 
+                    [5, 50, 5, 1]]
+    
+    print("The value of the current work schedule is:", recursiveSolution(workSchedule, n))
     
     print("-----")
     
