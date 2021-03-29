944bfe65481b8ad1ca6d2f7dfde018a055f837c3
Michael Buffone <44306347+BuffoneM@users.noreply.github.com>
2021-03-29 16:09:01 -0400

    +

---
+++ Assignments/Assignment 4/question2.py
@@ -0,0 +1,14 @@
+###
+# Michael Buffone
+# March 29th, 2021
+# COSC4106 Assignment 4 : Question 2
+#
+###
+     
+def main():
+    print("-----")
+    
+    print("-----")
+    
+if __name__ == "__main__":
+    main()
