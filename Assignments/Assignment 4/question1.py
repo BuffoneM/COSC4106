8bf6b312db310fb9e174de55d5fdc04d87d2cc6c
Michael Buffone <44306347+BuffoneM@users.noreply.github.com>
2021-03-29 13:07:26 -0400

    +

---
+++ Assignments/Assignment 4/question1.py
@@ -0,0 +1,55 @@
+###
+# Michael Buffone
+# March 27th, 2021
+# COSC4106 Assignment 4 : Question 1
+#
+# Write a program to solve the Longest Common Subsequence problem using dynamic programming
+# as discussed in class. For example if the input is:
+# X = ABCBDAB
+# Y = BDCABA
+# Output = BCBA
+###
+
+def print2dArray(x, y, m, n, array):
+    # Print the top values
+    print("  ", end=" ")
+    for i in range(0, n):
+        print(y[i], end="  ")
+    print()
+    # Print the arrays
+    for i in range(0, m):
+        print(x[i], array[i])
+        
+def subsequenceSolver(x, y, array, m, n):    
+   for i in range(0, m):
+       for j in range(0, n):
+           if x[i] == y[j]:
+               array[i][j] = array[i-1][j-1] + 1
+           else:
+               array[i][j] = max(array[i-1][j], array[i][j-1])
+                              
+    print2dArray(x, y, m, n, array)
+        
+
+def main():
+    print("-----")
+    
+    x = "xABCB"
+    y = "yBDCAB"
+    m = len(x)
+    n = len(y)
+    array = [[0 for i in range(n)] for j in range(m)]
+    
+    print("X string:", x[1:])
+    print("Y string:", y[1:])
+    print("m =", m-1)
+    print("n =", n-1)
+    print()
+    print2dArray(x, y, m, n, array)
+    print("Solving longest common subsequence for Y and Y:")
+    subsequenceSolver(x, y, array, m, n)
+    
+    print("-----")
+    
+if __name__ == "__main__":
+    main()
---
+++ Assignments/Assignment 4/tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+
+                array[i][j] = max(array[i-1][j], array[i][j-1])
